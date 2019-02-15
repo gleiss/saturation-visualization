4e12a64fde8468c3d20ca609efd643e845289647
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2019-02-15 14:40:50 +0100

    implement filterNonConsequences (#26)

--- proof_visualization/model/transformations.py
+++ proof_visualization/model/transformations.py
@@ -40,3 +40,57 @@ def filterNonParents(dag, relevantIds):
 				remainingLeaves.add(currentNodeId)
 
 	return Dag(remainingNodes, remainingLeaves)
+
+def filterNonConsequences(dag, relevantIds):
+	assert isinstance(dag, Dag)
+	assert isinstance(relevantIds, set)
+	for relevantId in relevantIds:
+		assert isinstance(relevantId, int)
+
+	# use new set to avoid mutating relevantIds
+	transitiveChildren = relevantIds
+
+	# need to compute remaining nodes and remaining leaves of new dag
+	remainingNodes = dict()
+	remainingLeaves = set()
+
+	# add all transitive children of ids in transitiveChildren to transitiveChildren
+	postOrderTraversal = DFPostOrderTraversal(dag)
+	while(postOrderTraversal.hasNext()):
+		currentNode = postOrderTraversal.next()
+		currentNodeId = currentNode.number
+		# check if currentNode occurs in transitiveChildren or 
+		# has a parent which occurs in transitiveChildren
+		isRelevant = (currentNodeId in transitiveChildren)
+		for parentId in currentNode.parents:
+			if parentId in transitiveChildren:
+				isRelevant = True
+	
+		if isRelevant:
+			# add its id to the set of relevant ids
+			transitiveChildren.add(currentNodeId)
+
+			# add a boundary node for each parent which doesn't occurs in transitiveChildren
+			for parentId in currentNode.parents:
+				if not parentId in transitiveChildren:
+					boundaryNode = createBoundaryNode(dag, dag.get(parentId))
+					boundaryNodeId = boundaryNode.number
+					
+					assert(not boundaryNodeId in dag.leaves) # boundaryNode has currentNode as child and is therefore no leaf 
+					remainingNodes[boundaryNodeId] = boundaryNode
+
+			# add node to remainingNodes
+			remainingNodes[currentNodeId] = currentNode
+
+			# if leaf, add node also to remainingLeaves
+			if currentNodeId in dag.leaves:
+				remainingLeaves.add(currentNodeId)
+	
+	return Dag(remainingNodes, remainingLeaves)
+
+# create a boundary node, which has the same id as the Node node, but as inference "Boundary" and no parents
+def createBoundaryNode(dag,node):
+	assert(isinstance(dag, Dag))
+	assert(isinstance(node, Node))
+
+	return Node(node.number, node.clause, "Boundary", [])
