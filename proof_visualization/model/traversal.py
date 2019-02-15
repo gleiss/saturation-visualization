8b4309745fec8a5e8817bd026465f382809aa8fd
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2019-02-15 09:34:16 +0100

    implement reversed post order traversal (#24)

--- proof_visualization/model/traversal.py
+++ proof_visualization/model/traversal.py
@@ -1,7 +1,9 @@
 from proof_visualization.model.dag import Dag
 from proof_visualization.model.node import Node
 
-class PostOrderTraversal:
+# iterator for traversing DAG, where each node is visited only after all parents are visited
+# implements standard iterative depth-first postorder traversal
+class DFPostOrderTraversal:
 	def __init__(self, dag):
 		self.dag = dag
 		self.todo = list(dag.leaves)
@@ -12,7 +14,6 @@ class PostOrderTraversal:
 
 	# returns the next node (the actual node, not its id) for traversal 
 	# only call after checking for hasNext
-	# implements standard iterative post order DAG-traversal
 	def next(self):
 		while len(self.todo) > 0:
 			currentId = self.todo[-1]
@@ -44,3 +45,20 @@ class PostOrderTraversal:
 		
 		# we have already iterated through all inferences, so next() should not have been called
 		assert(False)
+
+# iterator for traversing DAG, where each node is visited before any parent node is visited
+# implemented as reversed postorder traversal
+class ReversePostOrderTraversal:
+	def __init__(self, dag):
+		# compute post order and save result in postOrder
+		it = DFPostOrderTraversal(dag)
+		self.postOrder = list()
+		while(it.hasNext()):
+			self.postOrder.append(it.next())
+
+	def hasNext(self):
+		return len(self.postOrder) > 0
+
+	def next(self):
+		assert(self.hasNext())
+		return self.postOrder.pop()
