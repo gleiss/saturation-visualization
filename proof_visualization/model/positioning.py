c5e736a17bfaced4e7fe0b916b8ec21aa94471da
Bernhard Gleiss <bgleiss@forsyte.tuwien.ac.at>
2019-03-05 10:11:14 +0100

    revert removal of pygraphviz call

--- proof_visualization/model/positioning.py
+++ proof_visualization/model/positioning.py
@@ -1,7 +1,7 @@
 import re
 from collections import namedtuple
 
-#import pygraphviz as pgv
+import pygraphviz as pgv
 
 PLAIN_PATTERN = re.compile(r'node (\d+) ([0-9.]+) ([0-9.]+).+')
 
@@ -11,19 +11,19 @@ NodePosition = namedtuple('NodePosition', ['id_', 'x_coord', 'y_coord'])
 def calculate_node_positions(proof_dag):
     """Use pygraphviz to calculate node positions and return the result as a list of NodePositions"""
 
-    #graph = pgv.AGraph(directed=True)
-#
-    #for node in proof_dag:
-    #    graph.add_node(node.number, label=node.clause)
-#
-    #for node in proof_dag:
-    #    for parent in node.parents:
-    #        graph.add_edge(parent, node.number)
-    #graph.layout(prog='dot')
+    graph = pgv.AGraph(directed=True)
+
+    for node in proof_dag:
+        graph.add_node(node.number, label=node.clause)
+
+    for node in proof_dag:
+       for parent in node.parents:
+           graph.add_edge(parent, node.number)
+    graph.layout(prog='dot')
 
     # need to save result in file, since that's how pygraphviz works
     file_name = 'graph.plain'
-    #graph.draw(file_name)
+    graph.draw(file_name)
 
     with open(file_name) as graph_positions:
         node_positions = []
