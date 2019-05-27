14c24ba11ef8f4a5e2b95e7b77cbe4331bde1339
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-05-28 00:00:20 +0200

    Add start scripts

---
+++ start_backend.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# activate venv before executing!
+pip install -r requirements.txt
+cd visualization-backend
+python app.py
---
+++ start_frontend.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd visualization-frontend
+yarn install && yarn start
