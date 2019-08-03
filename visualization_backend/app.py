db9e8895c9913568762fed875515643ebae89532
Lena Schnedlitz <lena.schnedlitz@mail.de>
2019-08-03 19:38:26 +0200

    Remove unused methods

--- visualization_backend/app.py
+++ visualization_backend/app.py
@@ -12,28 +12,12 @@ app.config.from_object(__name__)
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    dag = init_with_example()
-    return json.dumps({'dag': dag.to_json()})
-
-
 @app.route('/', methods=['POST'])
 def handle_file_upload():
     request_params = request.get_json()
     text = request_params.get('file', '')
-    dag = to_dag(text)
+    dag = process(text)
     return json.dumps({'dag': dag.to_json()})
-
-
-def init_with_example():
-    with open('example.proof') as proof_file:
-        file_content = proof_file.read()
-        return to_dag(file_content)
-
-
-def to_dag(file_content):
-    return process(file_content)
 
 
 if __name__ == '__main__':
