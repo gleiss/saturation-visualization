5b77a5d0e10a50b00a5a8f19d4b2985a2e7744ca
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2019-11-08 16:46:28 -0500

    fix backend startup script

--- start_backend.sh
+++ start_backend.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-
-# activate venv before executing!
+PROJ_ROOT=$PWD
+Z3_PATH=~/opt/z3/build/python/
+Z3_BIN=~/opt/z3m/bin/z3
+git clone git@github.com:nhamlv-55/deepSpacer.git
+cd deepSpacer
+echo "Installing requirements"
 pip3 install -r requirements.txt
-cd visualization_backend
-python3 app.py
+echo "running the Backend server"
+cd pobvis/app
+export PYTHONPATH=$Z3_PATH:$PROJ_ROOT/deepSpacer/
+echo $PYTHONPATH
+python3 main_sat_vis.py -z3 $Z3_BIN
