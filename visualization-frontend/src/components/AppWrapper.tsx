191203f1b8c4a0347df5ddb59ba54b0966ab072a
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-03-06 14:25:46 -0500

    clean

--- visualization-frontend/src/components/AppWrapper.tsx
+++ visualization-frontend/src/components/AppWrapper.tsx
@@ -14,28 +14,27 @@ type Props = {
 	  orientClauses: boolean,
  	  varNames: string
 };
-  
-  export class AppWrapper extends React.Component<Props, {}> {
-	
-	render() {
-		  return (
-			    <div id="appWrapper">
-				      <NavigationBar 
-					    name={this.props.name}
-				      />
-              <App 
-                  name = {this.props.name}
-                  problem={this.props.problem}
-                  exp_path = {this.props.exp_path}
-                  spacerUserOptions={this.props.spacerUserOptions}
-                  mode={this.props.mode}
-                  hideBracketsAssoc={this.props.hideBracketsAssoc}
-                  nonStrictForNegatedStrictInequalities={this.props.nonStrictForNegatedStrictInequalities}
-                  orientClauses={this.props.orientClauses}
-                  varNames={this.props.varNames}
-              />
-          </div>
-		  )
-	}
-  }
-  
+
+export class AppWrapper extends React.Component<Props, {}> {
+	  render() {
+		    return (
+			      <div id="appWrapper">
+				        <NavigationBar 
+					          name={this.props.name}
+				        />
+                <App 
+                    name = {this.props.name}
+                    problem={this.props.problem}
+                    exp_path = {this.props.exp_path}
+                    spacerUserOptions={this.props.spacerUserOptions}
+                    mode={this.props.mode}
+                    hideBracketsAssoc={this.props.hideBracketsAssoc}
+                    nonStrictForNegatedStrictInequalities={this.props.nonStrictForNegatedStrictInequalities}
+                    orientClauses={this.props.orientClauses}
+                    varNames={this.props.varNames}
+                />
+            </div>
+		    )
+	  }
+}
+
