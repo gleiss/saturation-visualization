6b53c1562615cd30465f48579bc7f07a6eafa9ee
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-03-06 14:16:58 -0500

    pass additional props to App

--- visualization-frontend/src/components/AppWrapper.tsx
+++ visualization-frontend/src/components/AppWrapper.tsx
@@ -4,15 +4,16 @@ import App from "./App";
 import '../styles/AppWrapper.css';
 
 type Props = {
-	name: string,
+	  name: string,
 	  problem: string,
     exp_path: string,
-	spacerUserOptions: string,
-	mode: "proof" | "replay" | "iterative"
-	hideBracketsAssoc: boolean,
-	nonStrictForNegatedStrictInequalities: boolean, 
-	orientClauses: boolean,
-  };
+	  spacerUserOptions: string,
+	  mode: "proof" | "replay" | "iterative"
+	  hideBracketsAssoc: boolean,
+	  nonStrictForNegatedStrictInequalities: boolean, 
+	  orientClauses: boolean,
+ 	  varNames: string
+};
   
   export class AppWrapper extends React.Component<Props, {}> {
 	
@@ -22,17 +23,18 @@ type Props = {
 				<NavigationBar 
 					name={this.props.name}
 				/>
-				<App 
-        name = {this.props.name}
-				problem={this.props.problem}
-        exp_path = {this.props.exp_path}
-					spacerUserOptions={this.props.spacerUserOptions}
-					mode={this.props.mode}
-					hideBracketsAssoc={this.props.hideBracketsAssoc}
-					nonStrictForNegatedStrictInequalities={this.props.nonStrictForNegatedStrictInequalities}
-					orientClauses={this.props.orientClauses}
-				/>
-			</div>
+        <App 
+            name = {this.props.name}
+            problem={this.props.problem}
+            exp_path = {this.props.exp_path}
+            spacerUserOptions={this.props.spacerUserOptions}
+            mode={this.props.mode}
+            hideBracketsAssoc={this.props.hideBracketsAssoc}
+            nonStrictForNegatedStrictInequalities={this.props.nonStrictForNegatedStrictInequalities}
+            orientClauses={this.props.orientClauses}
+            varNames={this.props.varNames}
+        />
+      </div>
 		)
 	}
   }
